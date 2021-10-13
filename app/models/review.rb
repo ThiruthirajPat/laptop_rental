cd2b22e9d5132969c3687e979cd73c971ef4afbe
Vick-Bay <vickramb@hotmail.com>
2021-10-13 16:19:39 +0400

    added seeds for laptop and validation for reviews

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,8 +1,9 @@
 class Review < ApplicationRecord
   belongs_to :booking
 
-  validates :rating, :content, presence: true
-
   # :rating => inclusion 1 to 5
   # :content => min length 10 characters
+
+  validates :rating, presence: true, inclusion: { in: 0..5 }, numericality: { only_integer: true }
+  validates :content, presence: true, length: { minimum: 10 }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,41 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts "Cleaning database..."
+Laptop.destroy_all
+
+puts "Creating laptops..."
+
+laptop1 = {
+            brand: "HP",
+            model: "HP1370",
+            operating_system: "Windows",
+            date_manufacture: Date.new(2020, 1, 7),
+            collection_point: "Curepipe",
+            rental_rate: "100",
+            user: User.create!({ email: "laptop1@hp.com" })
+          }
+
+laptop2 = {
+            brand: "Apple",
+            model: "Macbook Pro",
+            operating_system: "Mac",
+            date_manufacture: Date.new(2020, 2, 7),
+            collection_point: "Port Louis",
+            rental_rate: "150",
+            user: User.create!({ email: "laptop2@mac.com" })
+          }
+
+laptop3 = {
+            brand: "Dell",
+            model: "Latitude",
+            operating_system: "Linux",
+            date_manufacture: Date.new(2020, 3, 7),
+            collection_point: "Quatre Bornes",
+            rental_rate: "70",
+            user: User.create!({ email: "laptop3@linux.com" })
+          }
+
+[laptop1, laptop2, laptop3].each do |attributes|
+  laptop = Laptop.create!(attributes)
+  puts "Created #{laptop.brand}"
+end
+
+puts "Finished!"
