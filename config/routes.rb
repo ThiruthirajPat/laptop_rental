1cd6012a3aaeec92b1c707c510ccae25b8b47279
Thiruthiraj Pather <thiruthiraj.pather@edhec.com>
2021-10-16 16:26:12 +0400

    creation of new booking

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   get 'bookings/index'
-  get 'bookings/new'
-  get 'bookings/create'
-  get 'bookings/show'
-  get 'bookings/edit'
+  # get 'bookings/new'
+  # get 'bookings/create'
+  # get 'bookings/show'
+  # get 'bookings/edit'
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
     resources :bookings, only: [:new, :create]
   end 
 
-  resources :bookings, only: [:index, :show, :edit, :update] do
+  resources :bookings, only: [:index, :show, :new, :create,  :edit, :update] do
     resources :reviews, only: [:new, :create]
   end
 
