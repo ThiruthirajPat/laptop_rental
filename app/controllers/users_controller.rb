28cc97afa5cadfa21ec6a9812e7f1370ef571198
Thiruthiraj Pather <thiruthiraj.pather@edhec.com>
2021-10-19 21:19:02 +0400

    added links to the account page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,16 @@ class UsersController < ApplicationController
   def show
     @user = current_user
   end
+
+  def edit
+    @user = current_user
+  end
+
+  def update
+    if current_user.update(user_params)
+      redirect_to current_user, notice: 'Account was updated'
+    else
+      render :edit
+    end
+  end
 end
