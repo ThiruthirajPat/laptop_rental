d91bb740a26fba6c32fb80010b1f799987809abd
Vickram Bayragee <vickramb@hotmail.com>
2021-10-20 14:51:31 +0400

    updated seed for users to add photo

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
+
+  has_one_attached :photo
+
   has_many :laptops
   has_many :bookings
   #has many :reviews :through =>:bookings
-  # validates :first_name, presence: true
-  # validates :last_name, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
 end
